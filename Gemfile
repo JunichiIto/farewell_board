f5adf2a0e6d8ddd2fd66f2c0192caf142d59e707
Junichi Ito <june.1st.717@gmail.com>
2012-05-20 22:06:32 +0900

    added bootstrap

--- Gemfile
+++ Gemfile
@@ -1,6 +1,8 @@
 source 'https://rubygems.org'
 
 gem 'rails', '3.2.3'
+gem "twitter-bootstrap-rails"
+gem 'therubyracer'
 
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 FarewellBoard2::Application.routes.draw do
+
   devise_for :users
 
   # The priority is based upon order of creation:
@@ -50,7 +51,7 @@ FarewellBoard2::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'pages#index'
 
   # See how all your routes lay out with "rake routes"
 
