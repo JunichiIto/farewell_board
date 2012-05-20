0a0764e4207b26bf54ac6402449c03ed29a5001d
Junichi Ito <june.1st.717@gmail.com>
2012-05-20 21:56:19 +0900

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FarewellBoard2::Application.config.secret_token = '30f9bc94e7f85e9d2ea84a55f091da553650fd5945eb2f8c7775be81beed8a622c68916c506a172e066fa24af28bfbf63c24bf4429e99a28d9c7d40f21f1c20e'
